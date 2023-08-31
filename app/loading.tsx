ab2cb1c5d7d81405efb9a9a58d4f3825d29f045d
mehranminaei <m.minaei84@gmail.com>
2023-08-31 10:58:26 +0300

    feat: add some folder struct and lib

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+const Loading = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <span>Loading...</span>
+    </div>
+  );
+};
+export default Loading;
